4871d12b439a82cdd974f309fd68621e10022992
minjae <minjae@minjaeui-MacBook-Pro.local>
2017-04-30 16:44:50 +0900

    [J]bookmarkView init

---
+++ Project/MachumCafe/MachumCafe/BookmarkViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BookmarkViewController.swift
+//  MachumCafe
+//
+//  Created by   minjae on 2017. 4. 28..
+//  Copyright © 2017년 Febrix. All rights reserved.
+//
+
+import UIKit
+
+class BookmarkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
