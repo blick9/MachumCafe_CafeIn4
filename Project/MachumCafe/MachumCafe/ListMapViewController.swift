90be6c026f502b162ec1f043a5ca8e2ff5bc0312
minjae <minjae@minjaeui-MacBook-Pro.local>
2017-04-30 17:15:19 +0900

    [J]GMSMapView연결 및 viewDidLoad 초기 생성값 정의

--- Project/MachumCafe/MachumCafe/ListMapViewController.swift
+++ Project/MachumCafe/MachumCafe/ListMapViewController.swift
@@ -12,13 +12,31 @@ import GooglePlaces
 import Alamofire
 import SwiftyJSON
 
-class ListMapViewController: UIViewController {
+class ListMapViewController: UIViewController,GMSMapViewDelegate,CLLocationManagerDelegate {
 
+    @IBOutlet weak var googleMap: GMSMapView!
+    
+    var locationManager = CLLocationManager()
+    var placesClient: GMSPlacesClient!
+    var currentLocation = [String: Double]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(#function, "Map")
 
-        // Do any additional setup after loading the view.
+        //locationManager - 좌표 알려주는 매니져
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingHeading()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startMonitoringSignificantLocationChanges()
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 37.404796, longitude: 127.106053, zoom: 15.0)
+        self.googleMap.camera = camera
+        self.googleMap.delegate = self
+        self.googleMap?.isMyLocationEnabled = true
+        self.googleMap.settings.myLocationButton = true
+        self.googleMap.settings.zoomGestures = true
     }
     
 }
