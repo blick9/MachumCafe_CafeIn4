79abb4f333813b3dc79531c90477baaaf269a5c0
minjae <minjae@minjaeui-MacBook-Pro.local>
2017-05-03 12:47:09 +0900

    [J]북마크와 디테일뷰 segue 연결 작업중

--- Project/MachumCafe/MachumCafe/BookmarkViewController.swift
+++ Project/MachumCafe/MachumCafe/BookmarkViewController.swift
@@ -11,8 +11,8 @@ import UIKit
 class BookmarkViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     var userId = String()
-    let tempArray = [[#imageLiteral(resourceName: "1"),#imageLiteral(resourceName: "2"),#imageLiteral(resourceName: "3"),#imageLiteral(resourceName: "4")],["압구정 사치커피", "스타벅스 역삼점", "카페 티", "클로이 뭐래츠"],["서울시 강남구 압구정동 471-47", "서울시 강남구 역삼동 714-28", "서울시 서초구 서초동 1024-5", "경기도 성남시 분당구 정자동 729번지"]]
-    
+    var userBookmark = [String]()
+
     override func viewWillAppear(_ animated: Bool) {
         // 네트워크에서 유저가 가진 북마크 목록 들고 오기
         getBookmarkList()
@@ -25,10 +25,10 @@ class BookmarkViewController: UIViewController {
     
     func getBookmarkList() {
         userId = User.sharedInstance.user.getUser()["id"] as! String
+        userBookmark = User.sharedInstance.user.getUser()["bookmark"] as! [String]
         NetworkBookmark.getMyBookmark(userId: userId) { (message, cafe, bookmarkID) in
             print(message)
             Cafe.sharedInstance.bookmarkList = cafe
-            print(Cafe.sharedInstance.bookmarkList)
             self.collectionView.reloadData()
         }
     }
@@ -45,7 +45,6 @@ extension BookmarkViewController : UICollectionViewDataSource, UICollectionViewD
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print(Cafe.sharedInstance.bookmarkList.count)
         return Cafe.sharedInstance.bookmarkList.count
     }
     
@@ -56,4 +55,26 @@ extension BookmarkViewController : UICollectionViewDataSource, UICollectionViewD
         cell.bookmarkCafeAddress.text = Cafe.sharedInstance.bookmarkList[indexPath.row].getCafe()["address"] as? String
              return cell
     }
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        print("You selected cell #\(indexPath.item)!")
+//        prepare(for: , sender: <#T##Any?#>)
+//    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DetailView" {
+            if let item = self.collectionView.indexPathsForSelectedItems{
+                let indexPath = item[0]
+                    print(indexPath.row)
+                let controller = segue.destination as! CafeDetailViewController
+                for cafe in Cafe.sharedInstance.cafeList {
+                    if let cafeID = cafe.getCafe()["id"] {
+                        if cafeID as! String == userBookmark[indexPath.row] {
+                            
+                        }
+                    }
+                }
+                // cafeId를 이용해서 디테일뷰 그릴 수 있게 네트워크카페 api가 있음 좋겠다.
+            }
+        }
+    }
+    
 }
