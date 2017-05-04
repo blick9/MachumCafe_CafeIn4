613b942332c32b8f09c3c878d9c295774069dcf5
HannaJeon <jks7psj@gmail.com>
2017-05-04 18:38:55 +0900

    [H] 로그아웃 네트워크 함수 추가 & 로그아웃 버튼 활성화

--- Project/MachumCafe/MachumCafe/NetworkCafe.swift
+++ Project/MachumCafe/MachumCafe/NetworkCafe.swift
@@ -45,7 +45,6 @@ class NetworkCafe {
     
     // MARK: 카페 이미지 데이터모델에 저장
     static func getImagesData(imagesName: [String], cafe: ModelCafe, callback: @escaping (_ imageData: Data) -> Void) {
-        
         for imageName in imagesName {
             Alamofire.request("\(url)/api/v1/cafe/\(imageName)").responseData(completionHandler: { (response) in
                 callback(response.result.value!)
