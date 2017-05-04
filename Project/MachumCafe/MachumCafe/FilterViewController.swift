4ed04d1f8b298fa5bea3e861d259edc1f1f7515d
minjae <minjae@minjaeui-MacBook-Pro.local>
2017-05-04 16:23:53 +0900

    [J]버튼 디자인 완료

--- Project/MachumCafe/MachumCafe/FilterViewController.swift
+++ Project/MachumCafe/MachumCafe/FilterViewController.swift
@@ -10,12 +10,44 @@ import UIKit
 
 class FilterViewController: UIViewController {
     
+    @IBOutlet weak var allnight: UIButton!
+    @IBOutlet weak var dessert: UIButton!
+    @IBOutlet weak var smoking: UIButton!
+    @IBOutlet weak var parkinglot: UIButton!
+    @IBOutlet weak var bathroom: UIButton!
+    @IBOutlet weak var meetingroom: UIButton!
+    var buttonArray = [UIButton]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "필터검색"
+        buttonArray = [allnight, dessert,smoking,parkinglot,bathroom,meetingroom]
+        designButtonColor(buttonArray: buttonArray)
+    }
+    
+    func designButtonColor(buttonArray : [UIButton]) {
+        for button in buttonArray {
+            button.layer.cornerRadius = 10
+            button.layer.borderWidth = 2
+            button.layer.borderColor = UIColor.init(red: 255, green: 232, blue: 129).cgColor
+            button.tintColor = UIColor.clear
+            button.setTitleColor(UIColor.init(red: 51, green: 51, blue: 51), for: .normal)
+            button.setTitleColor(UIColor.init(red: 51, green: 51, blue: 51), for: .selected)
+            button.setBackgroundImage(#imageLiteral(resourceName: "backgrondColor"), for: .selected)
+            button.addTarget(self, action: #selector(pickFilter(_:)) , for: .touchUpInside)
+        }
+    }
+    func pickFilter(_ button: UIButton) {
+        button.isSelected = !button.isSelected
+        print(button.currentTitle)
         
     }
 
+    
+    
+
+    
     @IBAction func closeButtonAction(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
