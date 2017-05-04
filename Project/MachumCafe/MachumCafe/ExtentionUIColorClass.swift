2203381195b6dd141d3d96d7504823b9b6c0ace4
Koo Chanwoo <febrix9@gmail.com>
2017-05-04 13:53:37 +0900

    [9] UIColor Hex 코드 Extension 추가

--- Project/MachumCafe/MachumCafe/ExtentionUIColorClass.swift
+++ Project/MachumCafe/MachumCafe/ExtentionUIColorClass.swift
@@ -16,4 +16,11 @@ extension UIColor {
         
         self.init(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
     }
+    
+    static func HexColor(rgbValue: UInt32, alpha: Double) -> UIColor {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8) / 256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+        return UIColor(red: red, green: green, blue: blue, alpha: CGFloat(alpha))
+    }
 }
