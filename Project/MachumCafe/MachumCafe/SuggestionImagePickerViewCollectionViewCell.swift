8cdd31f83a5abaae5df674b6ab7b10040e979ec3
= <=>
2017-05-11 12:15:40 +0900

    [B] 충돌 해결

---
+++ Project/MachumCafe/MachumCafe/SuggestionImagePickerViewCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SuggestionImagePickerViewCollectionViewCell.swift
+//  MachumCafe
+//
+//  Created by Danb on 2017. 5. 11..
+//  Copyright © 2017년 Febrix. All rights reserved.
+//
+
+import UIKit
+
+class SuggestionImagePickerViewCollectionViewCell: UICollectionViewCell {
+    
+}
