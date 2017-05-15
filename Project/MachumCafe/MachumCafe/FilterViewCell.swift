c57226bdb29d9884e1b2748270361d0b97867932
= <=>
2017-05-16 00:18:24 +0900

    [B] selected deselected 구현, layoutflow 추가

--- Project/MachumCafe/MachumCafe/FilterViewCell.swift
+++ Project/MachumCafe/MachumCafe/FilterViewCell.swift
@@ -12,12 +12,13 @@ class FilterViewCell: UICollectionViewCell {
     @IBOutlet weak var category: UILabel!
     
     override func awakeFromNib() {
+        super.awakeFromNib()
         isSelected = false
     }
     
     override var isSelected: Bool {
         didSet {
-            category.backgroundColor = isSelected ? UIColor(red: 255, green: 232, blue: 129) : UIColor.clear
+            self.backgroundColor = isSelected ? UIColor(red: 255, green: 232, blue: 129) : UIColor.clear
         }
     }
     
--- Project/MachumCafe/MachumCafe/FilterViewController.swift
+++ Project/MachumCafe/MachumCafe/FilterViewController.swift
@@ -11,12 +11,15 @@ import UIKit
 class FilterViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var multiple = true
 
     var categoryArray = ["24시","연중무휴","주차","편한의자","좌식","모임","미팅룸","스터디","넓은공간","아이와함께","디저트","베이커리","로스팅","산책로","모닥불","드라이브","북카페","이색카페","야경","조용한","고급스러운","여유로운","힐링"]
     var filterArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView?.allowsMultipleSelection = multiple
         self.navigationItem.title = "필터검색"
         collectionView.delegate = self
         collectionView.dataSource = self
@@ -49,10 +52,13 @@ class FilterViewController: UIViewController {
         let selectedItem = collectionView.indexPathsForSelectedItems
         for item in selectedItem! {
             collectionView.deselectItem(at: item , animated: false)
+            filterArray.removeAll()
         }
         //cell.category.isSelected == false
         //filterArray.removeAll()
-            }
+        print(filterArray)
+
+    }
     
     @IBAction func closeButtonAction(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -62,7 +68,7 @@ class FilterViewController: UIViewController {
     }
 }
 
-extension FilterViewController : UICollectionViewDataSource, UICollectionViewDelegate {
+extension FilterViewController : UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
@@ -76,20 +82,27 @@ extension FilterViewController : UICollectionViewDataSource, UICollectionViewDel
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! FilterViewCell
-        
-        cell.layer.cornerRadius = 10
+        cell.sizeToFit()
+        cell.layer.cornerRadius = cell.frame.height/2
         cell.layer.borderWidth = 2
         cell.layer.borderColor = UIColor.init(red: 255, green: 232, blue: 129).cgColor
         cell.category.text = categoryArray[indexPath.row]
         cell.category.textColor = UIColor.init(red: 51, green: 51, blue: 51)
+        cell.category.sizeToFit()
        // cell.category.setBackgroundImage(#imageLiteral(resourceName: "backgrondColor"), for: .selected)
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! FilterViewCell
+        guard multiple else {
+            return
+        }
         cell.isSelected = !cell.isSelected
+        filterArray.append(categoryArray[indexPath.row])
+        print(filterArray)
+
         
-        print(cell.isSelected)
+        // print(cell.isSelected)
        // print(indexPath.row)
 //        if cell.isSelected == true {
 //            cell.isSelected = !cell.isSelected
@@ -101,19 +114,33 @@ extension FilterViewController : UICollectionViewDataSource, UICollectionViewDel
 //        print(filterArray)
     }
     
-//    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! FilterViewCell
-//        let categoryIndex = categoryArray[indexPath.row]
-//        if let index = filterArray.index(of: categoryIndex) {
-//            filterArray.remove(at: index)
-//        }
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! FilterViewCell
+        let categoryIndex = categoryArray[indexPath.row]
+        if let index = filterArray.index(of: categoryIndex) {
+            filterArray.remove(at: index)
+        }
+        print(filterArray)
 //        for i in 0..<filterArray.count {
 //            if cell.category.text == filterArray[i] {
 //                self.filterArray.remove(at: i)
 //                break
 //            }
 //        }
-//        print(filterArray)
-//    }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        collectionView.viewWithTag(0)?.sizeToFit()
+        let width = collectionView.frame.width / 4 - 1
+        return CGSize(width: width, height: 35.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20.0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+
 }
 
