9eea407613fbccb1b8e8a85a6788a70789249ed7
Koo Chanwoo <febrix9@gmail.com>
2017-05-15 13:37:09 +0900

    [9] View 배경에 따라 Status Bar Style 변경

--- Project/MachumCafe/MachumCafe/SignUpViewController.swift
+++ Project/MachumCafe/MachumCafe/SignUpViewController.swift
@@ -15,23 +15,20 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var cofirmPasswordTextField: UITextField!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-//        navigationController?.isNavigationBarHidden = true
     }
     
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIApplication.shared.statusBarStyle = .default
     }
+    
     @IBAction func backButtonAction(_ sender: Any) {
 //        self.navigationController?.popViewController(animated: true)
         self.dismiss(animated: true, completion: nil)
     }
-    
     
     @IBAction func signUpButton(_ sender: Any) {
         
@@ -51,7 +48,6 @@ class SignUpViewController: UIViewController {
             }
         }
     }
-    
 }
 
 extension String {
