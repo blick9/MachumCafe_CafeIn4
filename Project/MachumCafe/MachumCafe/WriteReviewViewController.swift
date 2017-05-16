1043a470ddee3b88ba06aa9302ea1c1d42762d58
minjae <minjae@minjaeui-MacBook-Pro.local>
2017-05-16 16:37:31 +0900

    [J]리뷰작성 만듬

---
+++ Project/MachumCafe/MachumCafe/WriteReviewViewController.swift
@@ -0,0 +1,43 @@
+//
+//  WriteReviewViewController.swift
+//  MachumCafe
+//
+//  Created by   minjae on 2017. 5. 16..
+//  Copyright © 2017년 Febrix. All rights reserved.
+//
+
+import UIKit
+
+class WriteReviewViewController: UIViewController {
+
+    @IBOutlet weak var writeReview: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func registReview(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelReview(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
