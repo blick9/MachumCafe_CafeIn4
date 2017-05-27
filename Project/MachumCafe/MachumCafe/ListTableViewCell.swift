91dd2f6297362b3f6feaca69ac3369ee7965ddd3
Febrix <Febrix@Febrixui-MacBook-Pro.local>
2017-05-27 16:23:41 +0900

    [9] Rating View 추가(임시)

--- Project/MachumCafe/MachumCafe/ListTableViewCell.swift
+++ Project/MachumCafe/MachumCafe/ListTableViewCell.swift
@@ -16,10 +16,15 @@ class ListTableViewCell: UITableViewCell {
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var bookmarkButton: UIButton!
     @IBOutlet weak var category: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var ratingStarImageView: UIImageView!
+    @IBOutlet weak var ratingView: UIView!
     
     override func layoutSubviews() {
         self.bookmarkButton.setImage(#imageLiteral(resourceName: "Bookmark_Bt"), for: .normal)
         self.bookmarkButton.setImage(#imageLiteral(resourceName: "Bookmarked_Bt"), for: .selected)
+        ratingLabel.layer.cornerRadius = 4
+        ratingLabel.layer.backgroundColor = UIColor(red: 255, green: 232, blue: 129).cgColor
     
     }
     
---
+++ Project/MachumCafe/MachumCafe/RatingView.swift
@@ -0,0 +1,21 @@
+//
+//  RatingView.swift
+//  MachumCafe
+//
+//  Created by Febrix on 2017. 5. 27..
+//  Copyright © 2017년 Febrix. All rights reserved.
+//
+
+import UIKit
+
+class RatingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
