f97204330d23edb632999420f79d881056a1a70f
Febrix <Febrix@Febrixui-MacBook-Pro.local>
2017-05-27 20:52:07 +0900

    [9] Model 변수명 변경으로 인한 코드 수정

--- Project/MachumCafe/MachumCafe/LogInViewController.swift
+++ Project/MachumCafe/MachumCafe/LogInViewController.swift
@@ -73,8 +73,8 @@ class LogInViewController: UIViewController {
             if result {
                 User.sharedInstance.user = user
                 User.sharedInstance.isUser = true
-                if !(user.getUser()["imageURL"] as! String).isEmpty {
-                    NetworkUser.getUserImage(imageURL: user.getUser()["imageURL"] as! String) { (imageData) in
+                if !(user.getUser()["profileImageURL"] as! String).isEmpty {
+                    NetworkUser.getUserImage(imageURL: user.getUser()["profileImageURL"] as! String) { (imageData) in
                         user.setProfileImage(profileImage: imageData)
                         self.dismiss(animated: true, completion: nil)
                     }
